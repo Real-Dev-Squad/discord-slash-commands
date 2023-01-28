fb310133cbf901034803bf0fe8e32253d5d96b58
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-01-29 04:22:50 +0530

    Add verify bot to set interactions URL

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import { InteractionResponseType, InteractionType } from "discord-interactions";
 import { Router } from "itty-router";
 import { Env } from "./typeDefinitions/default.types";
+import { discordMessageRequest } from "./typeDefinitions/discordMessage.types";
+import JsonResponse from "./utils/JsonResponse";
+import { verifyBot } from "./utils/verifyBot";
 
 const router = Router();
 
@@ -7,6 +11,16 @@ router.get("/", async () => {
   return new Response(`{message: "Welcome to our discord Bot Server 👋"}`, {
     status: 200,
   });
+});
+
+router.post("/", async (request) => {
+  const message: discordMessageRequest = await request.json();
+  if (message.type === InteractionType.PING) {
+    return new JsonResponse({
+      type: InteractionResponseType.PONG,
+    });
+  }
+  return new JsonResponse({ error: "Unknown Interaction" }, { status: 400 });
 });
 
 router.all("*", async () => {
@@ -17,6 +31,11 @@ router.all("*", async () => {
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
+    const isVerifiedRequest = await verifyBot(request, env);
+    if (!isVerifiedRequest) {
+      console.error("Invalid Request");
+      return new Response("Bad Request signature.", { status: 401 });
+    }
     return router.handle(request, env);
   },
 };
---
+++ src/utils/verifyBot.ts
@@ -0,0 +1,24 @@
+import { verifyKey } from "discord-interactions";
+import { Env } from "../typeDefinitions/default.types";
+
+export async function verifyBot(request: Request, env: Env) {
+  if (request.method === "POST") {
+    const signature = request.headers.get("x-signature-ed25519");
+    const timestamp = request.headers.get("x-signature-timestamp");
+    const body = await request.clone().arrayBuffer();
+
+    if (signature === null || timestamp === null) {
+      return new Response("Bad Request signature.", { status: 401 });
+    }
+
+    const isValidRequest = verifyKey(
+      body,
+      signature,
+      timestamp,
+      env.DISCORD_PUBLIC_KEY
+    );
+    return isValidRequest;
+  } else {
+    return false;
+  }
+}
