506bd1e90bf9c6e9a2d9e33fe15513079f2ca302
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-01-29 20:08:54 +0530

    add errors in constants

---
+++ src/constants/errors.ts
@@ -0,0 +1,11 @@
+export const UNKNOWN_INTERACTION = {
+  error: "Unknown Interaction",
+};
+
+export const NOT_FOUND = {
+  error: "🥹 oops! No fish 🐟 caught 🎣",
+};
+
+export const BAD_SIGNATURE = {
+  error: "🚫 Bad Request Signature",
+};
