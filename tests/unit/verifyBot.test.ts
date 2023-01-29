6f2ea0d5fb00a250ae1dfc23c3c9d4207b6f1da6
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-01-29 22:19:06 +0530

    resolve typos

--- tests/unit/verifyBot.test.ts
+++
@@ -1,9 +0,0 @@
-import { verifyBot } from "../../src/utils/verifyBot";
-
-describe("test verifyBot function", () => {
-  it("should return false for invalid request", async () => {
-    const request = new Request("https://test.com");
-    const response = await verifyBot(request, {});
-    expect(response).toBe(false);
-  });
-});
