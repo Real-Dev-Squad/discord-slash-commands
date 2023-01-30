619f558d501dee52c5e7114e0e16f7905469f908
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-01-30 11:24:48 +0530

    import response to avoid test fail

--- tests/unit/commandNotFoundHandler.test.ts
+++
@@ -1,8 +0,0 @@
-import { commandNotFound } from "../../src/controllers/commandNotFound";
-import JSONResponse from "../../src/utils/JsonResponse";
-
-describe("Test Command Not Found Handler", () => {
-  it("Returns JSONResponse for unknown commands", () => {
-    expect(commandNotFound()).toBeInstanceOf(JSONResponse);
-  });
-});
--- tests/unit/helloCommandHandler.test.ts
+++
@@ -1,9 +0,0 @@
-import { helloCommand } from "../../src/controllers/helloCommand";
-import JSONResponse from "../../src/utils/JsonResponse";
-
-describe("Test Hello Command Handler", () => {
-  it("Returns response of type JSONResponse", () => {
-    const response = helloCommand(123456);
-    expect(response).toBeInstanceOf(JSONResponse);
-  });
-});
