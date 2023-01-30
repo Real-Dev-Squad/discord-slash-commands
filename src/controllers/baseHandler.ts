31983641548e7557fa3b51de763fda9428f79dca
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-01-30 16:05:49 +0530

    add return types

--- src/controllers/baseHandler.ts
+++ src/controllers/baseHandler.ts
@@ -1,10 +1,11 @@
 import { HELLO_COMMAND } from "../constants/commands";
 import { discordMessageRequest } from "../typeDefinitions/discordMessage.types";
 import { getCommandName } from "../utils/getCommandName";
+import JSONResponse from "../utils/JsonResponse";
 import { commandNotFound } from "./commandNotFound";
 import { helloCommand } from "./helloCommand";
 
-export function baseHandler(message: discordMessageRequest) {
+export function baseHandler(message: discordMessageRequest): JSONResponse {
   const command = message.data.name.toLowerCase();
   switch (command) {
     case getCommandName(HELLO_COMMAND): {
--- src/utils/discordResponse.ts
+++ src/utils/discordResponse.ts
@@ -1,6 +1,6 @@
 import JSONResponse from "./JsonResponse";
 
-export const DISCORD_RESPONSE = (reply: string) => {
+export const DISCORD_RESPONSE = (reply: string): JSONResponse => {
   return new JSONResponse({
     type: 4,
     data: {
