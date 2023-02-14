3981269fcbbf1b8a61e8d8e0952dcb9950d13d72
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-02-14 18:45:58 +0530

    rename verification_site constant to verification_site_url

--- src/controllers/verifyCommand.ts
+++ src/controllers/verifyCommand.ts
@@ -1,4 +1,4 @@
-import { VERIFICATION_SITE } from "../constants/urls";
+import { VERIFICATION_SITE_URL } from "../constants/urls";
 import { env } from "../typeDefinitions/default.types";
 import { discordTextResponse } from "../utils/discordResponse";
 import { generateUniqueToken } from "../utils/generateUniqueToken";
@@ -6,7 +6,7 @@ import { sendDiscordDm } from "../utils/sendDiscordDm";
 
 export async function verifyCommand(userId: number, env: env) {
   const token = await generateUniqueToken();
-  const url = `${VERIFICATION_SITE}/?token=${token}`;
+  const url = `${VERIFICATION_SITE_URL}/?token=${token}`;
   await sendDiscordDm(userId, env, url);
   return discordTextResponse("Please check the DM");
 }
