6438b24e943fec9b15de6660779e1f9d1b4dac57
Samyak Shah <samyakshah3008@gmail.com>
2023-02-25 00:07:05 +0530

    refactored

--- src/utils/sendUserDiscordData.ts
+++ src/utils/sendUserDiscordData.ts
@@ -1,14 +1,15 @@
 import { env } from "../typeDefinitions/default.types";
 import jwt from "@tsndr/cloudflare-worker-jwt";
+import { RDS_STAGING_API_URL } from "../constants/urls";
 
-export const storeUniqueToken = async (
+export const sendUserDiscordData = async (
   token: string,
   discordId: number,
   env: env
 ) => {
-  const jwtToken = await jwt.sign(
+  const authToken = await jwt.sign(
     { name: "Cloudflare Worker", exp: Math.floor(Date.now() / 1000) + 15 },
-    env.PRIVATE_TOKEN,
+    env.DISCORD_PRIVATE_KEY,
     { algorithm: "RS256" }
   );
   const data = {
@@ -21,17 +22,14 @@ export const storeUniqueToken = async (
   };
 
   try {
-    const response = await fetch(
-      `https://staging-api.realdevsquad.com/external-accounts`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bot ${jwtToken}`,
-        },
-        body: JSON.stringify(data),
-      }
-    );
+    const response = await fetch(`${RDS_STAGING_API_URL}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${authToken}`,
+      },
+      body: JSON.stringify(data),
+    });
     return response.json();
   } catch (err) {
     console.log("Could not store the token in RDS Backend. Error: ", err);
