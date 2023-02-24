6161bf7588728371b81427fb024afbf3787f7a41
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-02-24 07:01:34 +0530

    add try catch and proper error message

--- src/utils/updateNickname.ts
+++ src/utils/updateNickname.ts
@@ -8,16 +8,20 @@ export async function updateNickName(
 ) {
   const changeNickNameURL = `${DISCORD_BASE_URL}/guilds/${env.DISCORD_GUILD_ID}/members/${discordId}`;
   const data = { nick: nickname };
-  const nameChangeResponse = await fetch(changeNickNameURL, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bot ${env.DISCORD_TOKEN}`,
-    },
-    body: JSON.stringify(data),
-  });
-  if (nameChangeResponse.ok) {
-    return await nameChangeResponse.json();
+  try {
+    const nameChangeResponse = await fetch(changeNickNameURL, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bot ${env.DISCORD_TOKEN}`,
+      },
+      body: JSON.stringify(data),
+    });
+    if (nameChangeResponse.ok) {
+      return await nameChangeResponse.json();
+    }
+    return "Some internal Server error occurred";
+  } catch (error) {
+    console.log(error);
   }
-  return "Failed";
 }
