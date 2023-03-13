5ebe3ff6d0b608e9ced83375999b19e8a0949c26
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-03-13 11:19:09 +0530

    add JWT Verification

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "itty-router";
 import { InteractionResponseType, InteractionType } from "discord-interactions";
+import jwt from "@tsndr/cloudflare-worker-jwt";
 import * as response from "./constants/responses";
 import { baseHandler } from "./controllers/baseHandler";
 import { env } from "./typeDefinitions/default.types";
@@ -18,10 +19,15 @@ router.get("/", async () => {
 
 router.patch("/", async (request, env) => {
   const req = await request.json();
-  const discord_id = req.discordId;
-  const nickname = req.username;
-  const res = await updateNickName(discord_id, nickname, env);
-  return new JSONResponse(res);
+  const authorization = await request.headers
+    .get("Authorization")
+    .split(" ")[1];
+  if (await jwt.verify(authorization, env.BOT_PRIVATE_KEY)) {
+    const discord_id = req.discordId;
+    const nickname = req.username;
+    const res = await updateNickName(discord_id, nickname, env);
+    return new JSONResponse(res);
+  } else return new JSONResponse(response.BAD_SIGNATURE);
 });
 
 router.post("/", async (request, env) => {
