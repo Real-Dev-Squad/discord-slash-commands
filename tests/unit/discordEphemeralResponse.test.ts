e4e6db571612cfe269314ab00f4b8ff188b9b9fe
Samyak Shah <samyakshah3008@gmail.com>
2023-04-08 02:27:12 +0530

    TEST ENHANCE: discordEphemeralResponse test suits update

--- tests/unit/discordEphemeralResponse.test.ts
+++ tests/unit/discordEphemeralResponse.test.ts
@@ -1,27 +1,43 @@
+import { InteractionResponseType } from "discord-interactions";
+import { responseJson } from "../../src/typeDefinitions/default.types";
 import JSONResponse from "../../src/utils/JsonResponse";
-import { discordephemeralResponse } from "../../src/utils/discordEphemeralResponse.ts";
+import { discordEphemeralResponse } from "../../src/utils/discordEphemeralResponse";
 
-describe("Test discordResponse function", () => {
-  const DISCORD_TOKEN = "DUMMY TOKEN";
+describe("Test discordEphemeralResponse function", () => {
   it("should return a JSONResponse", () => {
-    const response = discordephemeralResponse(
-      "Dummy token message",
-      DISCORD_TOKEN
-    );
-    expect(response).toBeInstanceOf(Promise<JSONResponse>);
+    const response = discordEphemeralResponse("Hello");
+    expect(response).toBeInstanceOf(JSONResponse);
   });
-  it("Should have response status 200", async () => {
-    const response = await discordephemeralResponse(
-      "Dummy token message",
-      DISCORD_TOKEN
+  it("Should have type as channelMessageWithSource", async () => {
+    const response: responseJson = await discordEphemeralResponse(
+      "Hello"
+    ).json();
+    expect(response?.type).toBe(
+      InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE
     );
-    expect(response?.status).toBe(200);
   });
-  it("Should have status text defined", async () => {
-    const response = await discordephemeralResponse(
-      "Dummy token message",
-      DISCORD_TOKEN
-    );
-    expect(response?.statusText).toBeDefined;
+  it("Should contain a content property in data", async () => {
+    const response: responseJson = await discordEphemeralResponse(
+      "Hello"
+    ).json();
+    expect(response?.data).toHaveProperty("content");
+  });
+  it("Should have content as hello", async () => {
+    const response: responseJson = await discordEphemeralResponse(
+      "Hello"
+    ).json();
+    expect(response?.data.content).toBe("Hello");
+  });
+  it("Should contain a flag property in data", async () => {
+    const response: responseJson = await discordEphemeralResponse(
+      "Hello"
+    ).json();
+    expect(response?.data).toHaveProperty("flags");
+  });
+  it("Should have flag as 64", async () => {
+    const response: responseJson = await discordEphemeralResponse(
+      "Hello"
+    ).json();
+    expect(response?.data.flags).toBe(64);
   });
 });
