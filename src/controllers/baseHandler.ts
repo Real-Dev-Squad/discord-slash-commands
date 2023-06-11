9e6440efc6d3368924d0dfeaf031db874a308530
vvaibhav <vaibhavdesai888@gmail.com>
2023-06-11 17:26:49 +0530

    chore: annoying linter

--- src/controllers/baseHandler.ts
+++ src/controllers/baseHandler.ts
@@ -7,14 +7,14 @@ import { lowerCaseMessageCommands } from "../utils/lowerCaseMessageCommand";
 import { commandNotFound } from "./commandNotFound";
 import { helloCommand } from "./helloCommand";
 import { verifyCommand } from "./verifyCommand";
-import {mentionEachUser} from './mentionEachUser'
+import { mentionEachUser } from "./mentionEachUser";
 
 export async function baseHandler(
   message: discordMessageRequest,
   env: env
 ): Promise<JSONResponse> {
   const command = lowerCaseMessageCommands(message);
-  
+
   switch (command) {
     case getCommandName(HELLO): {
       return helloCommand(message.member.user.id);
@@ -29,13 +29,16 @@ export async function baseHandler(
       );
     }
     case getCommandName(MENTION_EACH): {
-      return await mentionEachUser({
-        //  assertion was added because the options type is optional also eslint had hard rules set so had to cross the border :) 
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        displayType: message.data?.options![0]?.name,
+      return await mentionEachUser(
+        {
+          //  assertion was added because the options type is optional also eslint had hard rules set so had to cross the border :)
           // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        options: message.data?.options![0]?.options![0]?.options,
-      },env)
+          displayType: message.data?.options![0]?.name,
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          options: message.data?.options![0]?.options![0]?.options,
+        },
+        env
+      );
     }
     default: {
       return commandNotFound();
