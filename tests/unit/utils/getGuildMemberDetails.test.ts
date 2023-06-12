8dcc424a98727a286b246bc45a965d269058ee66
DashDeipayan <deipayan.dash8@gmail.com>
2023-06-13 00:37:47 +0530

    added new test

--- tests/unit/utils/getGuildMemberDetails.test.ts
+++ tests/unit/utils/getGuildMemberDetails.test.ts
@@ -1,9 +1,8 @@
 import { DISCORD_BASE_URL } from "../../../src/constants/urls";
-import JSONResponse from "../../../src/utils/JsonResponse";
 import { getGuildMemberDetails } from "../../../src/utils/getGuildMemberDetails";
 import { dummyGuildMemberDetails } from "../../fixtures/fixture";
 
-describe("get guild member details", () => {
+describe("getGuildMemberDetails", () => {
   const mockEnv = {
     BOT_PUBLIC_KEY: "xyz",
     DISCORD_GUILD_ID: "123",
@@ -11,17 +10,17 @@ describe("get guild member details", () => {
   };
   const mockUserId = "12345678";
   const GET_GUILD_MEMBER_URL = `${DISCORD_BASE_URL}/guilds/${mockEnv.DISCORD_GUILD_ID}/members/${mockUserId}`;
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
 
-  test("returns JSON response with member's details on success", async () => {
-    const expectedResponse = dummyGuildMemberDetails;
+  test("returns member details on success", async () => {
+    jest.spyOn(global, "fetch").mockResolvedValueOnce({
+      ok: true,
+      json: jest.fn().mockResolvedValueOnce(dummyGuildMemberDetails),
+    } as unknown as Response);
 
-    jest
-      .spyOn(global, "fetch")
-      .mockImplementation(() =>
-        Promise.resolve(new JSONResponse(expectedResponse))
-      );
-
-    const response = await getGuildMemberDetails(mockUserId, mockEnv);
+    const result = await getGuildMemberDetails(mockUserId, mockEnv);
 
     expect(global.fetch).toHaveBeenCalledWith(GET_GUILD_MEMBER_URL, {
       method: "GET",
@@ -31,6 +30,46 @@ describe("get guild member details", () => {
       },
     });
 
-    expect(response).toEqual(expectedResponse);
+    expect(result).toEqual(dummyGuildMemberDetails);
+  });
+
+  test("returns INTERNAL_SERVER_ERROR on non-ok response", async () => {
+    jest.spyOn(global, "fetch").mockResolvedValueOnce({
+      ok: false,
+    } as unknown as Response);
+
+    const result = await getGuildMemberDetails(mockUserId, mockEnv);
+
+    expect(global.fetch).toHaveBeenCalledWith(GET_GUILD_MEMBER_URL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bot ${mockEnv.DISCORD_TOKEN}`,
+      },
+    });
+
+    expect(result).toEqual(
+      "Oops! We have encountered an internal Server Error"
+    );
+  });
+
+  test("returns INTERNAL_SERVER_ERROR on fetch error", async () => {
+    jest
+      .spyOn(global, "fetch")
+      .mockRejectedValueOnce(new Error("Network error"));
+
+    const result = await getGuildMemberDetails(mockUserId, mockEnv);
+
+    expect(global.fetch).toHaveBeenCalledWith(GET_GUILD_MEMBER_URL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bot ${mockEnv.DISCORD_TOKEN}`,
+      },
+    });
+
+    expect(result).toEqual(
+      "Oops! We have encountered an internal Server Error"
+    );
   });
 });
