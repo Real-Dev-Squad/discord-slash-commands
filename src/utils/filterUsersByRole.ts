c4e264dfe81cf5c1f60fda5cd71caa363c610d32
vvaibhav <vaibhavdesai888@gmail.com>
2023-06-13 20:02:47 +0530

    refactor: resolving PR comments

--- src/utils/filterUsersByRole.ts
+++ src/utils/filterUsersByRole.ts
@@ -5,12 +5,13 @@ export function filterUserByRoles(userArray: UserArray[], roleId: string) {
 
   if (userArray?.length > 0 && Array.isArray(userArray)) {
     filteredUser = userArray?.reduce((acc: string[], current: UserArray) => {
-      // role present in user ?
+     
       if (current.roles.includes(roleId)) {
-        // if yes ? then mod the user id & push in array
         const modedTag = `<@${current.user.id}>`;
+     
         acc.push(modedTag);
       }
+     
       return acc;
     }, []);
   }
