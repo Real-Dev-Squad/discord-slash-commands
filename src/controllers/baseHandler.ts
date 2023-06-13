953e6f1255b1a97ae0437436b86d3a3ef9ab3418
vvaibhav <vaibhavdesai888@gmail.com>
2023-06-13 20:29:23 +0530

    refactor: sorting imports as well as fix for pr comments

--- src/controllers/baseHandler.ts
+++ src/controllers/baseHandler.ts
@@ -1,13 +1,16 @@
-import { HELLO, MENTION_EACH, VERIFY } from "../constants/commands";
-import { env } from "../typeDefinitions/default.types";
-import { discordMessageRequest } from "../typeDefinitions/discordMessage.types";
-import { getCommandName } from "../utils/getCommandName";
-import JSONResponse from "../utils/JsonResponse";
-import { lowerCaseMessageCommands } from "../utils/lowerCaseMessageCommand";
 import { commandNotFound } from "./commandNotFound";
 import { helloCommand } from "./helloCommand";
 import { verifyCommand } from "./verifyCommand";
 import { mentionEachUser } from "./mentionEachUser";
+
+import { getCommandName } from "../utils/getCommandName";
+import JSONResponse from "../utils/JsonResponse";
+import { lowerCaseMessageCommands } from "../utils/lowerCaseMessageCommand";
+
+import { env } from "../typeDefinitions/default.types";
+import { discordMessageRequest, messageRequestDataOptions } from "../typeDefinitions/discordMessage.types";
+
+import { HELLO, MENTION_EACH, VERIFY } from "../constants/commands";
 
 export async function baseHandler(
   message: discordMessageRequest,
@@ -29,13 +32,13 @@ export async function baseHandler(
       );
     }
     case getCommandName(MENTION_EACH): {
+     const data = message.data?.options as Array<messageRequestDataOptions>;
+
       return await mentionEachUser(
         {
           //  assertion was added because the options type is optional also eslint had hard rules set so had to cross the border :)
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          displayType: message.data?.options![0]?.name,
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          options: message.data?.options![0]?.options![0]?.options,
+          displayType: data[0].name,
+          options: data[0].options[0].options,
         },
         env
       );
