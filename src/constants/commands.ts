ab993f59f81d25685f5ec40d5ea4897b35863209
Vaibhav Desai <vaibhavdesai888@gmail.com>
2023-06-22 23:12:52 +0530

    feat: rework on existing mention-each command (#76)
    * feat: changing format of command
    
    * refactor: formattingd
    
    * feat: changed input argument
    
    * feat: rework on existing code based on input
    
    * fix:output for changing the input
    
    * refactor: rework on test :(
    
    * refactor:test constants to fixtures
    
    * chore: linting
    
    * feat:added comments
    
    * chore: linting

--- src/constants/commands.ts
+++ src/constants/commands.ts
@@ -10,61 +10,20 @@ export const VERIFY = {
 };
 
 export const MENTION_EACH = {
-  name: "mention",
+  name: "mention-each",
   description: "mention each user with this role",
   options: [
     {
-      name: "list",
-      description: "get user in list",
-      type: 2, // 2 is type SUB_COMMAND_GROUP
-      options: [
-        {
-          name: "get",
-          description: "get me the role ",
-          type: 1, // 1 is type SUB_COMMAND
-          options: [
-            {
-              name: "user",
-              description: "The user to get",
-              type: 9, // 6 is type USER
-              required: true,
-            },
-            {
-              name: "message",
-              description: "What message to send to user ?",
-              type: 3, // 7 is type CHANNEL
-              required: true,
-            },
-          ],
-        },
-      ],
+      name: "role",
+      description: "to be tagged in the message",
+      type: 8, // 2 is type SUB_COMMAND_GROUP
+      required: true,
     },
     {
-      name: "series",
-      description: "get user in series",
-      type: 2,
-      options: [
-        {
-          name: "get",
-          description: "get me the role",
-          type: 1,
-          options: [
-            {
-              name: "user",
-              description: "The user to get",
-              type: 9, // 8 is type ROLE
-              required: true,
-            },
-            {
-              name: "message",
-              description:
-                "The channel permissions to get. If omitted, the guild permissions will be returned",
-              type: 3,
-              required: true,
-            },
-          ],
-        },
-      ],
+      name: "message",
+      description: "any message for them?",
+      type: 3,
+      require: false,
     },
   ],
 };
