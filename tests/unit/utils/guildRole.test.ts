3cae4b30739b60f7c11eb8b60577f6c388bc4af3
ritikjaiswal75 <ritik12103032000@gmail.com>
2023-06-24 21:24:54 +0530

    add test for remove guild role api

--- tests/unit/utils/guildRole.test.ts
+++ tests/unit/utils/guildRole.test.ts
@@ -1,6 +1,10 @@
 import JSONResponse from "../../../src/utils/JsonResponse";
 import * as response from "../../../src/constants/responses";
-import { createGuildRole, addGroupRole } from "../../../src/utils/guildRole";
+import {
+  createGuildRole,
+  addGroupRole,
+  removeGuildRole,
+} from "../../../src/utils/guildRole";
 import {
   dummyAddRoleBody,
   dummyCreateBody,
@@ -87,5 +91,41 @@ describe("addGroupRole", () => {
         },
       }
     );
+  });
+});
+
+describe("removeGuildRole", () => {
+  test("Should return success message on proper response", async () => {
+    const mockResponse = {
+      ok: true,
+    };
+    jest
+      .spyOn(global, "fetch")
+      .mockImplementation(() =>
+        Promise.resolve(new JSONResponse(mockResponse))
+      );
+    const result = await removeGuildRole(dummyAddRoleBody, guildEnv);
+    expect(global.fetch).toHaveBeenCalledWith(
+      `https://discord.com/api/v10/guilds/${guildEnv.DISCORD_GUILD_ID}/members/${dummyAddRoleBody.userid}/roles/${dummyAddRoleBody.roleid}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bot ${guildEnv.DISCORD_TOKEN}`,
+        },
+      }
+    );
+    expect(result).toEqual({
+      message: response.ROLE_REMOVED,
+      userAffected: {
+        userid: dummyAddRoleBody.userid,
+        roleid: dummyAddRoleBody.roleid,
+      },
+    });
+  });
+  test("Should return internal error response on api failure", async () => {
+    jest.spyOn(global, "fetch").mockRejectedValue("Oops some error");
+    const result = await removeGuildRole(dummyAddRoleBody, guildEnv);
+    expect(result).toEqual(response.INTERNAL_SERVER_ERROR);
   });
 });
