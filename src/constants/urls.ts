ab8f6b6961d8bc2e032155073566b4859cf60378
ritikjaiswal75 <ritik12103032000@gmail.com>
2023-06-24 14:15:36 +0530

    revert commits to urls

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,7 +1,6 @@
 export const RDS_BASE_API_URL = "https://api.realdevsquad.com";
 export const RDS_BASE_STAGING_API_URL = "https://staging-api.realdevsquad.com";
-export const RDS_BASE_DEVELOPMENT_API_URL =
-  "https://4904-2409-40f2-15-f890-e077-6751-acb1-b762.eu.ngrok.io"; // If needed, modify the URL to your local API server
+export const RDS_BASE_DEVELOPMENT_API_URL = "YOUR_LOCAL_API_URL"; // If needed, modify the URL to your local API server
 
 export const DISCORD_BASE_URL = "https://discord.com/api/v10";
 export const DISCORD_AVATAR_BASE_URL = "https://cdn.discordapp.com/avatars";
