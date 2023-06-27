a1903ffb936dee8b14d3a97d0a263618d4e0014a
Ritik Jaiswal <57758447+RitikJaiswal75@users.noreply.github.com>
2023-06-27 19:56:43 +0530

    Feature: Create Discord API Endpoint to Remove Role from User (#82)
    * add an api endpoint to relete role on discord
    
    * revert commits to urls
    
    * add test for remove guild role api
    
    * remove unwanted changes
    
    * add proper response headers

--- src/utils/guildRole.ts
+++ src/utils/guildRole.ts
@@ -1,4 +1,8 @@
-import { INTERNAL_SERVER_ERROR, ROLE_ADDED } from "../constants/responses";
+import {
+  INTERNAL_SERVER_ERROR,
+  ROLE_ADDED,
+  ROLE_REMOVED,
+} from "../constants/responses";
 import { DISCORD_BASE_URL } from "../constants/urls";
 import { env } from "../typeDefinitions/default.types";
 import {
@@ -50,6 +54,31 @@ export async function addGroupRole(body: memberGroupRole, env: env) {
       return { message: ROLE_ADDED };
     } else {
       return INTERNAL_SERVER_ERROR;
+    }
+  } catch (err) {
+    return INTERNAL_SERVER_ERROR;
+  }
+}
+
+export async function removeGuildRole(details: memberGroupRole, env: env) {
+  const { userid, roleid } = details;
+  const removeGuildRoleUrl = `${DISCORD_BASE_URL}/guilds/${env.DISCORD_GUILD_ID}/members/${userid}/roles/${roleid}`;
+  try {
+    const response = await fetch(removeGuildRoleUrl, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bot ${env.DISCORD_TOKEN}`,
+      },
+    });
+    if (response.ok) {
+      return {
+        message: ROLE_REMOVED,
+        userAffected: {
+          userid,
+          roleid,
+        },
+      };
     }
   } catch (err) {
     return INTERNAL_SERVER_ERROR;
