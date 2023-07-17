4af3672b1197daf61f87dd443a156d46e20ddbe6
shreya-mishra <shreyamishra040398@gmail.com>
2023-07-17 12:44:59 +0530

    raising different PR for /verify string

--- src/controllers/verifyCommand.ts
+++ src/controllers/verifyCommand.ts
@@ -1,5 +1,5 @@
 import config from "../../config/config";
-import { RETRY_COMMAND } from "../constants/responses";
+import { RETRY_COMMAND, VERIFICATION_STRING } from "../constants/responses";
 import { env } from "../typeDefinitions/default.types";
 import { discordEphemeralResponse } from "../utils/discordEphemeralResponse";
 import { generateUniqueToken } from "../utils/generateUniqueToken";
@@ -24,7 +24,7 @@ export async function verifyCommand(
   );
   if (response?.status === 201 || response?.status === 200) {
     const verificationSiteURL = config(env).VERIFICATION_SITE_URL;
-    const message = `${verificationSiteURL}/discord?token=${token}`;
+    const message = `${verificationSiteURL}/discord?token=${token}\n${VERIFICATION_STRING}`;;
     return discordEphemeralResponse(message);
   } else {
     return discordEphemeralResponse(RETRY_COMMAND);
