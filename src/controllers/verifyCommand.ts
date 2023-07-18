63e3d5037f5a8ae7a51724ad39b261b1a6190934
shreya-mishra <shreyamishra040398@gmail.com>
2023-07-18 23:02:25 +0530

    resolved comment

--- src/controllers/verifyCommand.ts
+++ src/controllers/verifyCommand.ts
@@ -23,7 +23,7 @@ export async function verifyCommand(
   if (response?.status === 201 || response?.status === 200) {
     const verificationSiteURL = config(env).VERIFICATION_SITE_URL;
     const message =
-      `${verificationSiteURL}/discord?token=${token}\n` + VERIFICATION_STRING;
+      `${verificationSiteURL}/discord?token=${token}\n${VERIFICATION_STRING}`;
     return discordEphemeralResponse(message);
   } else {
     return discordEphemeralResponse(RETRY_COMMAND);
