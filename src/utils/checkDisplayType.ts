5a3b0faea214ad912f7b2426c0e99366e1d6a114
shreya-mishra <shreyamishra040398@gmail.com>
2023-08-17 01:58:52 +0530

    format-fix

--- src/utils/checkDisplayType.ts
+++ src/utils/checkDisplayType.ts
@@ -7,4 +7,12 @@ export function checkDisplayType({
 }: {
   msgToBeSent?: string;
   usersWithMatchingRole: string[];
-})
+}) {
+  if (usersWithMatchingRole.length > 0) {
+    const returnString = msgToBeSent ? msgToBeSent : "";
+
+    return `${returnString} ${usersWithMatchingRole} \n \`Disclaimer: Very soon all the users will be tagged individually in a separate(new) message!\``;
+  } else {
+    return `Sorry no user found under this role.`;
+  }
+}
