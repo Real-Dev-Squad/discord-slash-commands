422a4b13160b633ab82db1b83fa676a382277977
SHREYA MISHRA <61110378+shreya-mishra@users.noreply.github.com>
2023-08-22 11:31:14 +0530

    /listening Command (#110)
    * initiated the work in discord slash listening command
    
    * initiated the work in discord slash listening command
    
    * resolved the issue
    
    * added testcase
    of remove listening
    
    * move copies to copy file
    
    * removed logs
    
    * resolved the comments
    
    * resolved the comments
    
    * resolved the comments

--- src/register.ts
+++ src/register.ts
@@ -1,4 +1,4 @@
-import { HELLO, MENTION_EACH, VERIFY } from "./constants/commands";
+import { HELLO, LISTENING, MENTION_EACH, VERIFY } from "./constants/commands";
 import { config } from "dotenv";
 import { DISCORD_BASE_URL } from "./constants/urls";
 import { registerCommands } from "./utils/registerCommands";
@@ -17,7 +17,7 @@ async function registerGuildCommands(
   discordApplicationId?: string,
   discordGuildId?: string
 ) {
-  const commands = [HELLO, VERIFY, MENTION_EACH];
+  const commands = [HELLO, VERIFY, MENTION_EACH, LISTENING];
 
   try {
     if (!discordBotToken) throw new Error("Please provide a BOT TOKEN");
