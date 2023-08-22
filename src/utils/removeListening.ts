b1b05e3e254cd037a9e42852983ed93c8c4c1fc2
shreya-mishra <shreyamishra040398@gmail.com>
2023-08-22 06:09:25 +0530

    resolved the comments

--- src/utils/removeListening.ts
+++ src/utils/removeListening.ts
@@ -1,7 +1,7 @@
 import { NICKNAME_SUFFIX } from "../constants/nicknameAffixes";
 
-export const removeListening = (nick: string) => {
-  const emojiRemovedNick = nick.replace("🎧", "");
+export const removeListening = (nickName: string) => {
+  const emojiRemovedNick = nickName.replace("🎧", "");
   const separator = NICKNAME_SUFFIX;
   const newNick = emojiRemovedNick.split(new RegExp(separator, "i"));
   return newNick[0].trim();
