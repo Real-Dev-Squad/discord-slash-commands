1efcc0c2e414aed588fd4dda53daeb83fff1906b
FMK2312 <fmkhandwala39@gmail.com>
2023-08-23 14:48:01 +0530

    updated type def for user backend

--- src/typeDefinitions/userBackend.types.d.ts
+++ src/typeDefinitions/userBackend.types.d.ts
@@ -5,21 +5,9 @@ export interface UserBackend {
     username: string;
     first_name: string;
     last_name: string;
-    email: string;
-    phone: number;
-    yoe: number;
-    company: string;
-    designation: string;
-    img: string;
     github_id: string;
-    linkedin_id: string;
     discordId: string;
-    twitter_id: string;
-    instagram_id: string;
-    website: string;
     github_display_name: string;
     isMember: boolean;
-    userType: string;
-    badges: [];
   };
 }
