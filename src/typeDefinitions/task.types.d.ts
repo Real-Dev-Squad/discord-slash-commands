34304081abedc50f0de82656895df865cff6c725
Sunny Sahsi <sahsisunny@gmail.com>
2023-09-06 16:20:42 +0530

    TEST: add test for formatTasks function

--- src/typeDefinitions/task.types.d.ts
+++ src/typeDefinitions/task.types.d.ts
@@ -1,20 +1,13 @@
 export type task = {
   id: string;
   title: string;
-  purpose: string;
-  featureUrl: string;
   type: string;
-  links: string[];
   endsOn: number;
-  startedOn: string;
   status: string;
   assignee?: string;
   percentCompleted: number;
   dependsOn: string[];
   participants?: string[];
-  completionAward: award;
-  lossRate: award;
-  isNoteworthy: boolean;
   createdBy: string;
   github?: {
     issue: {
