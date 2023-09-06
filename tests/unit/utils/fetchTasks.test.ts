b1daa4ded58d709aefd5fbc9f1e6279a1c38e44b
Sunny Sahsi <sahsisunny@gmail.com>
2023-09-06 13:17:49 +0530

    TEST: add test for fetchTasks

---
+++ tests/unit/utils/fetchTasks.test.ts
@@ -0,0 +1,24 @@
+import JSONResponse from "../../../src/utils/JsonResponse";
+import { fetchTasks } from "../../../src/utils/fetchTasks";
+import { tasksResponse } from "../../fixtures/tasks";
+
+describe("Test fetchTasks function", () => {
+  it("Should return a response", async () => {
+    const tasks = await fetchTasks("sunny-s", "IN_PROGRESS");
+    expect(tasks).toBeDefined();
+  });
+
+  it("Should return a response with tasks", async () => {
+    const mockResponse = tasksResponse;
+    jest
+      .spyOn(global, "fetch")
+      .mockImplementation(() =>
+        Promise.resolve(new JSONResponse(mockResponse))
+      );
+    const tasks = await fetchTasks("sunny-s", "IN_PROGRESS");
+    expect(tasks).toEqual(tasksResponse);
+    expect(global.fetch).toHaveBeenCalledWith(
+      "https://api.realdevsquad.com/tasks?status=IN_PROGRESS&assignee=sunny-s&dev=true"
+    );
+  });
+});
