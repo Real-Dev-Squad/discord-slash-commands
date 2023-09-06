378cc7d0e68e6d9bbd6af12c0f8e20e15a6f7720
Sunny Sahsi <sahsisunny@gmail.com>
2023-09-06 11:35:25 +0530

    TEST: add tet for formatDate and formatStatusToTitleCase function

---
+++ tests/unit/utils/formatDate.test.ts
@@ -0,0 +1,13 @@
+import { formatDate } from "../../../src/utils/formatDate";
+
+describe("Test formatDate function", () => {
+  it("Should return a string", () => {
+    const date = formatDate(1627708800);
+    expect(typeof date).toBe("string");
+  });
+
+  it("Should return a date in the format of 31 July 2021, 10:50 AM IST", () => {
+    const date = formatDate(1627708800);
+    expect(date).toBe("31 July 2021, 10:50 AM IST");
+  });
+});
