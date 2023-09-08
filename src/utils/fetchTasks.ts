8d985dcc04ee317539bf928655f9d145290e324d
Sunny Sahsi <sahsisunny@gmail.com>
2023-09-08 08:27:51 +0530

    FEATURE: Add /task command (#127)
    * FEATURE: add code changes for /task command
    
    * feat(utils): add formatDate and formatTask utility
    
    * feat(message): update response format
    
    * FEATURE: add code changes for /task command
    
    * FEAT: add TASK command to register
    
    * Update src/utils/getNickName.ts
    
    * REFACTOR: change var name and add try-catch to getNickName fun
    
    * TEST: add tet for formatDate and formatStatusToTitleCase function
    
    * REFACTOR: extract fetchTasks fun
    
    * TEST: add test for fetchTasks
    
    * TEST(FIX): fix formatDate test
    
    * TEST: improve test coverage of fetchTasks function
    
    * TEST: add test for formatTasks function
    
    * TEST: fix failed test
    
    * TEST: fix failed test
    
    * remove console from getNickName.ts
    
    * TEST: add test for fetchTask funtion
    
    * REFACTOR: changes position of status var
    
    * REFACTOR: add try catch to fetchTasks function
    
    * FIX: lint

---
+++ src/utils/fetchTasks.ts
@@ -0,0 +1,22 @@
+import { FAILED_TO_FETCH_TASKS } from "../constants/responses";
+import { RDS_BASE_API_URL } from "../constants/urls";
+import { TasksResponseType } from "../typeDefinitions/task.types";
+
+async function fetchTasks(assignee: string, status: string) {
+  try {
+    const url = `${RDS_BASE_API_URL}/tasks?status=${status}&assignee=${assignee}&dev=true`;
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(FAILED_TO_FETCH_TASKS.replace("{{assignee}}", assignee));
+    }
+
+    const responseData: TasksResponseType = await response.json();
+    return responseData;
+  } catch (error) {
+    console.error("An error occurred while fetching tasks:", error);
+    throw error;
+  }
+}
+
+export { fetchTasks };
