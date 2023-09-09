e57368f9f49c27576ab4bf5967220c446e24358d
Vinit <vinit224488@gmail.com>
2023-09-09 06:33:23 +0530

    fix failing test on github actions

--- tests/unit/utils/formatDate.test.ts
+++ tests/unit/utils/formatDate.test.ts
@@ -8,6 +8,6 @@ describe("Test formatDate function", () => {
 
   it("Should return a date in the format", () => {
     const date = formatDate(1627708800);
-    expect(date).toBe("31 July 2021, 10:50 AM IST");
+    expect(date).toBe("31 July 2021, 5:20 AM IST");
   });
 });
