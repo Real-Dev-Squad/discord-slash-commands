677e1e516c7d461542bef15c90b0ea38141f77dc
Jyotsna Mehta <53934353+j24m@users.noreply.github.com>
2023-09-14 18:13:26 +0530

    Minor fix

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,9 +1,6 @@
-export const RDS_BASE_API_URL =
-  "https://3a3d-2405-201-300f-281b-d5b-9550-274-fd5b.ngrok-free.app";
-export const RDS_BASE_STAGING_API_URL =
-  "https://3a3d-2405-201-300f-281b-d5b-9550-274-fd5b.ngrok-free.app";
-export const RDS_BASE_DEVELOPMENT_API_URL =
-  "https://3a3d-2405-201-300f-281b-d5b-9550-274-fd5b.ngrok-free.app"; // If needed, modify the URL to your local API server run through ngrok
+export const RDS_BASE_API_URL = "https://api.realdevsquad.com";
+export const RDS_BASE_STAGING_API_URL = "https://staging-api.realdevsquad.com";
+export const RDS_BASE_DEVELOPMENT_API_URL = "http://localhost:3000"; // If needed, modify the URL to your local API server run through ngrok
 
 export const DISCORD_BASE_URL = "https://discord.com/api/v10";
 export const DISCORD_AVATAR_BASE_URL = "https://cdn.discordapp.com/avatars";
