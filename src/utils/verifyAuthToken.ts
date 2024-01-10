7d09d30713867609ecdd7e239831612f9668bd86
Ajeyakrishna <98796547+Ajeyakrishna-k@users.noreply.github.com>
2024-01-11 01:04:12 +0530

    Adds a function to verify cron jobs auth token (#181)
    * feat: adds new function to verify cron jobs auth token
    
    * chore: renames key names
    
    * feat: adds null check

--- src/utils/verifyAuthToken.ts
+++ src/utils/verifyAuthToken.ts
@@ -24,3 +24,25 @@ export async function verifyAuthToken(authHeader: string, env: env) {
     throw new Error(AUTHENTICATION_ERROR);
   }
 }
+
+/**
+ *
+ * @param authHeader { string } : the auth header of request
+ * @param env { env }: the ctx (context) which contains the secrets put in as wrangler secrets.
+ */
+export async function verifyCronJobsToken(authHeader: string, env: env) {
+  if (!authHeader) {
+    throw new Error(INVALID_TOKEN_FORMAT);
+  }
+  const authHeaderParts = authHeader.split(" ");
+  if (authHeaderParts.length !== 2 || authHeaderParts[0] !== "Bearer") {
+    throw new Error(INVALID_TOKEN_FORMAT);
+  }
+  const authToken = authHeaderParts[1];
+  const isValid = await jwt.verify(authToken, env.CRON_JOBS_PUBLIC_KEY, {
+    algorithm: "RS256",
+  });
+  if (!isValid) {
+    throw new Error(AUTHENTICATION_ERROR);
+  }
+}
