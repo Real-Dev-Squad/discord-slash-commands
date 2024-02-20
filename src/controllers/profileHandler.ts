eb092bca404ba03082c33302eb45785517e8e3e2
Lakshay Manchanda <lakshaymanchanda73@gmail.com>
2024-02-21 00:27:54 +0530

    Code Cleaning

--- src/controllers/profileHandler.ts
+++ src/controllers/profileHandler.ts
@@ -11,7 +11,8 @@ export const sendProfileBlockedMessage = async (request: any, env: env) => {
   }
   try {
     await verifyAuthToken(authHeader, env);
-    const messageRequest: any = await request.json();
+    const messageRequest: { userId: string; reason: string } =
+      await request.json();
     const { userId, reason } = messageRequest;
     await sendProfileServiceBlockedMessage(userId, reason, env);
     return new JSONResponse("Message sent in tracking channel on discord");
