0f8080a95abcc39b1f794c754e0fe9db64499e23
Lakshay Manchanda <lakshaymanchanda73@gmail.com>
2024-02-21 02:20:07 +0530

    Added Auth for Identity Service

--- src/controllers/profileHandler.ts
+++ src/controllers/profileHandler.ts
@@ -2,7 +2,7 @@ import { env } from "../typeDefinitions/default.types";
 import { sendProfileServiceBlockedMessage } from "../utils/sendProfileServiceBlockedMessage";
 import JSONResponse from "../utils/JsonResponse";
 import * as response from "../constants/responses";
-import { verifyNodejsBackendAuthToken } from "../utils/verifyAuthToken";
+import { verifyIdentityServiceAuthToken } from "../utils/verifyAuthToken";
 
 export const sendProfileBlockedMessage = async (request: any, env: env) => {
   const authHeader = request.headers.get("Authorization");
@@ -10,7 +10,7 @@ export const sendProfileBlockedMessage = async (request: any, env: env) => {
     return new JSONResponse(response.BAD_SIGNATURE);
   }
   try {
-    await verifyNodejsBackendAuthToken(authHeader, env);
+    await verifyIdentityServiceAuthToken(authHeader, env);
     const messageRequest: { userId: string; reason: string } =
       await request.json();
     const { userId, reason } = messageRequest;
