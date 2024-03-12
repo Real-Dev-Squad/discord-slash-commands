506c5723ad82db1960b90c39c3ff20d7df585612
Achintya-Chatterjee <achintyachatterjee.jara@gmail.com>
2024-03-13 04:15:20 +0530

    refactor: add space

--- src/controllers/baseHandler.ts
+++ src/controllers/baseHandler.ts
@@ -10,11 +10,13 @@ import { muteUser, unmuteUser } from "../utils/userMuteUnmuteActions";
 import { getCommandName } from "../utils/getCommandName";
 import JSONResponse from "../utils/JsonResponse";
 import { lowerCaseMessageCommands } from "../utils/lowerCaseMessageCommand";
+
 import { env } from "../typeDefinitions/default.types";
 import {
   discordMessageRequest,
   messageRequestDataOptions,
 } from "../typeDefinitions/discordMessage.types";
+
 import {
   HELLO,
   LISTENING,
@@ -60,10 +62,13 @@ export async function baseHandler(
     }
     case getCommandName(MENTION_EACH): {
       const data = message.data?.options as Array<messageRequestDataOptions>;
+      // data[0] is role obj
+      // data[1] is message obj
       const transformedArgument = {
         roleToBeTaggedObj: data[0],
         displayMessageObj: data[1] ?? {},
       };
+
       return await mentionEachUser(transformedArgument, env);
     }
     case getCommandName(LISTENING): {
