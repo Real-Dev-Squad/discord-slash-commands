6fac2a029b2d0ffd2576ade63370ac3b6c841370
Sunny Sahsi <sahsisunny@gmail.com>
2024-04-13 07:47:05 +0530

    refactor: change API endpoint and related logic to get overdue task users who are in server

--- src/handlers/scheduledEventHandler.ts
+++ src/handlers/scheduledEventHandler.ts
@@ -1,24 +1,26 @@
 import { env } from "../typeDefinitions/default.types";
 import { taskOverDueDiscordMembers } from "../utils/taskOverDueDiscordMembers";
-import * as error from "../constants/responses";
-import { getDiscordIds } from "../utils/getDiscordIds";
 import config from "../../config/config";
 import { SUPER_USER_ONE, SUPER_USER_TWO } from "../constants/variables";
 
 export async function send(env: env): Promise<void> {
   try {
-    const assigneeIds: string[] | string = await taskOverDueDiscordMembers();
+    let discordIds: string[] | string = await taskOverDueDiscordMembers();
 
-    //A user might have more than one task which are running red
-    //so to mention them just once, we are using Set to filter out
-    const discordIds: string[] | string = await getDiscordIds(assigneeIds);
-    const uniqueDiscordIds = [...new Set(discordIds)];
+    if (!Array.isArray(discordIds)) {
+      // If it's not an array, convert it to an array with a single element
+      discordIds = [discordIds];
+    }
+
+    if (discordIds.length === 0) {
+      return;
+    }
 
     //notifying the two users with the authority.
     let stringToBeSent = `<@${SUPER_USER_ONE}> <@${SUPER_USER_TWO}>\nThese people have their task running red:\n`;
 
     let forFormatting = 0;
-    uniqueDiscordIds.forEach((id) => {
+    discordIds.forEach((id: string) => {
       const discordUser = `<@${id}> `;
       stringToBeSent += discordUser;
       forFormatting++;
@@ -35,7 +37,7 @@ export async function send(env: env): Promise<void> {
 
     const url = config(env).TRACKING_CHANNEL_URL;
 
-    const res = await fetch(url, {
+    await fetch(url, {
       method: "POST",
       body: JSON.stringify(bodyObj),
       headers: {
