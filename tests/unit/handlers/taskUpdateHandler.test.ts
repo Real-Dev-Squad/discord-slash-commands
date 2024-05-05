adcc1d2b99e8c09baa5a1d7f2ea0f260563e9509
TEJAS <98630752+tejaskh3@users.noreply.github.com>
2024-05-06 01:35:34 +0530

    add taskTitle field to /task/update API (#227)
    * add taskTitle field to /task/update API
    
    * feat:add test case for sendTaskUpdate util
    
    * feat: add test cases to taskUpdateHandler
    
    * refactor: add message in constant responses
    
    * remove: extra line

--- tests/unit/handlers/taskUpdateHandler.test.ts
+++ tests/unit/handlers/taskUpdateHandler.test.ts
@@ -2,7 +2,6 @@ import { sendTaskUpdatesHandler } from "../../../src/controllers/taskUpdatesHand
 import JSONResponse from "../../../src/utils/JsonResponse";
 import * as response from "../../../src/constants/responses";
 import { sendTaskUpdate } from "../../../src/utils/sendTaskUpdates";
-
 import { generateDummyRequestObject } from "../../fixtures/fixture";
 
 jest.mock("../../../src/utils/verifyAuthToken", () => ({
@@ -11,7 +10,6 @@ jest.mock("../../../src/utils/verifyAuthToken", () => ({
 jest.mock("../../../src/utils/sendTaskUpdates", () => ({
   sendTaskUpdate: jest.fn().mockResolvedValue(undefined),
 }));
-
 describe("sendTaskUpdatesHandler", () => {
   const mockEnv = { DISCORD_TOKEN: "mockToken" };
   const mockData = {
@@ -21,20 +19,22 @@ describe("sendTaskUpdatesHandler", () => {
       blockers: "NA",
       userName: "12345678910",
       taskId: "79wMEIek990",
+      taskTitle: "Hyperlink as task Title",
     },
   };
   afterEach(() => {
     jest.clearAllMocks();
   });
-
   it("sendTaskUpdate function should return undefined after successfully sending the message", async () => {
-    const { completed, planned, blockers, userName, taskId } = mockData.content;
+    const { completed, planned, blockers, userName, taskId, taskTitle } =
+      mockData.content;
     const response = await sendTaskUpdate(
       completed,
       planned,
       blockers,
       userName,
       taskId,
+      taskTitle,
       mockEnv
     );
     expect(response).toBe(undefined);
@@ -49,5 +49,23 @@ describe("sendTaskUpdatesHandler", () => {
     const jsonResponse: { error: string } = await result.json();
     expect(result.status).toBe(401);
     expect(jsonResponse).toEqual(response.UNAUTHORIZED);
+  });
+  it("should return success response if task update is sent successfully", async () => {
+    const mockRequest = generateDummyRequestObject({
+      url: "/task/update",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer dummyToken",
+      },
+    });
+    mockRequest.json = jest.fn().mockResolvedValue(mockData);
+    const result: JSONResponse = await sendTaskUpdatesHandler(
+      mockRequest,
+      mockEnv
+    );
+    expect(result.status).toBe(200);
+    const res: JSONResponse = await result.json();
+    expect(res).toBe(response.TASK_UPDATE_SENT_MESSAGE);
   });
 });
