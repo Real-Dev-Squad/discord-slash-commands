dffed182239475e880c8efb2f957e30c8abcc2c3
RishiChaubey31 <cenationrishi31@gmail.com>
2024-09-20 02:35:21 +0530

    feat: display message after running mention each command on Discord

--- src/controllers/mentionEachUser.ts
+++ src/controllers/mentionEachUser.ts
@@ -25,32 +25,41 @@ export async function mentionEachUser(
   const roleId = transformedArgument.roleToBeTaggedObj.value;
   const msgToBeSent = transformedArgument?.displayMessageObj?.value;
   const dev = transformedArgument?.dev?.value || false;
-  // optional chaining here only because display message obj is optional argument
+
   const usersWithMatchingRole = filterUserByRoles(
     getMembersInServerResponse as UserArray[],
     roleId
   );
+
   const payload = {
     channelId: transformedArgument.channelId,
     roleId: roleId,
     message: msgToBeSent,
     usersWithMatchingRole,
   };
+
+  // Construct the message with the role mention and user list
+  const roleTag = `<@&${roleId}>`;
+  let responseMessage;
+  if (usersWithMatchingRole.length === 0) {
+    responseMessage = `No users found with the ${roleTag} role.`;
+  } else {
+    const userList = usersWithMatchingRole.join(", ");
+    responseMessage = `The users with ${roleTag} roles are: ${userList}`;
+  }
+
   if (!dev || usersWithMatchingRole.length === 0) {
-    const responseData = checkDisplayType({
-      usersWithMatchingRole,
-      msgToBeSent,
-    });
-    return discordTextResponse(responseData);
+    return discordTextResponse(responseMessage);
   } else {
     ctx.waitUntil(
       mentionEachUserInMessage({
-        message: payload.message,
+        message: responseMessage,
         userIds: payload.usersWithMatchingRole,
         channelId: payload.channelId,
         env,
       })
     );
+    
     return discordTextResponse(
       `Found ${usersWithMatchingRole.length} users with matched role, mentioning them shortly...`
     );
