6997e7cc568afbf07eae75ec9348172bb28bbf25
pallabez <pallabsonowal999@gmail.com>
2024-10-02 14:50:21 +0530

    fix tests

--- tests/fixtures/groups.ts
+++ tests/fixtures/groups.ts
@@ -4,3 +4,8 @@ export const group: DiscordGuildRole = {
   id: "1",
   name: "group-frontend",
 };
+
+export const invalidGroup: DiscordGuildRole = {
+  id: "2",
+  name: "invalidRole",
+};
--- tests/unit/handlers/groupInvite.test.ts
+++ tests/unit/handlers/groupInvite.test.ts
@@ -2,34 +2,35 @@ import { environment } from "../../fixtures/config";
 import * as DiscordGroups from "../../../src/utils/fetchDiscordGroupById";
 import JSONResponse from "../../../src/utils/JsonResponse";
 import { groupInvite } from "../../../src/controllers/groupInvite";
-import { group } from "../../fixtures/groups";
+import { group, invalidGroup } from "../../fixtures/groups";
 import { discordTextResponse } from "../../../src/utils/discordResponse";
 
 describe("Test /group-invite command", () => {
-  beforeEach(() => {
-    jest
-      .spyOn(DiscordGroups, "fetchDiscordGroupById")
-      .mockImplementation(() => Promise.resolve(group));
-  });
-
   afterEach(() => {
     jest.resetAllMocks();
     jest.restoreAllMocks();
   });
 
   it("Should be an instance of JSONResponse", async () => {
+    jest
+      .spyOn(DiscordGroups, "fetchDiscordGroupById")
+      .mockImplementation(() => Promise.resolve(group));
+
     const response = await groupInvite("1", group.id, environment[0]);
 
     expect(response).toBeInstanceOf(JSONResponse);
   });
 
   it("Should return a discordTextResponse if group is not found", async () => {
-    const invalidRoleId = "invalidRoleId";
+    jest
+      .spyOn(DiscordGroups, "fetchDiscordGroupById")
+      .mockImplementation(() => Promise.resolve(invalidGroup));
+
     const expectedResponse = discordTextResponse(
-      `<@&${invalidRoleId}> is not a valid group.`
+      `<@&${invalidGroup.id}> is not a valid group.`
     );
 
-    const response = await groupInvite("1", invalidRoleId, environment[0]);
+    const response = await groupInvite("1", invalidGroup.id, environment[0]);
 
     expect(await response.json()).toEqual(await expectedResponse.json());
   });
