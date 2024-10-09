316a17d73ccef351a2b5410a988943f65600b1be
Shubham Raj <shubhamxraj10@gmail.com>
2024-10-09 23:16:41 +0530

    fix:lint

--- src/controllers/baseHandler.ts
+++ src/controllers/baseHandler.ts
@@ -82,7 +82,7 @@ export async function baseHandler(
      * HOT FIX to prevent non superusers from running the /remove commmand.
      * More info :- https://discord.com/channels/673083527624916993/729399523268624405/1293604361758441605
      * ---
-    */
+     */
     // case getCommandName(REMOVE): {
     //   const data = message.data?.options as Array<messageRequestDataOptions>;
     //   const transformedArgument = {
