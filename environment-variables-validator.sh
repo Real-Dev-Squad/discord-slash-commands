863b3a02e319ca2deb6e72f9fe5267fb4a585f2a
Tharun <45945950+Saitharun279@users.noreply.github.com>
2024-10-16 00:48:35 +0530

    issue#257 | Add env var to store env var names

--- environment-variables-validator.sh
+++ environment-variables-validator.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-ENV_VARS_FILE="environment-variables.txt"
-# Check if the file exists
-if [[ ! -f "$ENV_VARS_FILE" ]]; then
-  echo "File $ENV_VARS_FILE does not exist."
-  exit 1
-fi
-
-# Read the file and iterate through each variable name
-while IFS= read -r var_name; do
+for var_name in $ENV_VAR_NAMES; do
   # Check if the variable is set in the environment
   if [[ -z "${!var_name}" ]]; then
     echo "Environment variable $var_name is not set."
     exit 1  # Exit with error if any variable is not set
   fi
-done < "$ENV_VARS_FILE"
+done
+echo "All Environment variables are set."
