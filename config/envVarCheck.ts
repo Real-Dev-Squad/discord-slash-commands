0a1523a3f63a978861bd2e224d9ff5aff0659a7d
Tharun <45945950+Saitharun279@users.noreply.github.com>
2024-11-07 00:33:40 +0530

    removed redundant comments

--- config/envVarCheck.ts
+++ config/envVarCheck.ts
@@ -7,13 +7,11 @@ import { loadEnv } from "./config";
  * ---
  */
 export function validateEnv() {
-  //pass empty object as env and fromWorkerEnv = false, since this method is should get executed in github actions and not in worker
   const env: env = loadEnv({}, false);
   const missingEnvVariables = Object.keys(env).filter(
     (key) => env[key] == ""
   );
 
-  // Logging missing environment variables and throw error if any are missing
   if (missingEnvVariables.length > 0) {
     throw new Error(
       `Missing environment variables: ${missingEnvVariables.join(", ")}`
